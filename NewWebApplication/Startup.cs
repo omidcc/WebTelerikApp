26e474f97337dd1508268c07d72d5fe6e3594cfc
omidcc <khairulomi@gmail.com>
2015-09-14 18:21:48 +0600

    version 1.0

---
+++ NewWebApplication/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NewWebApplication.Startup))]
+namespace NewWebApplication
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
